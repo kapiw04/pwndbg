ef811bcef2479615b24e3e20c15d5256072d6434
Disconnect3d <dominik.b.czarnota@gmail.com>
2024-11-27 16:49:23 +0100

    Fix lint

--- pwndbg/commands/strings.py
+++ pwndbg/commands/strings.py
@@ -10,24 +10,34 @@ import pwndbg.commands
 from pwndbg.commands import CommandCategory
 from pwndbg.lib.memory import Page
 
-parser = argparse.ArgumentParser(description="Extracts and displays ASCII strings from readable memory pages of the debugged process.")
+parser = argparse.ArgumentParser(
+    description="Extracts and displays ASCII strings from readable memory pages of the debugged process."
+)
 
+parser.add_argument("-n", type=int, default=4, help="Minimum length of ASCII strings to include")
 parser.add_argument(
-    "-n", type=int, default=4, help="Minimum length of ASCII strings to include")
+    "page_names",
+    type=str,
+    nargs="*",
+    default=[],
+    help="Mapping to search [e.g. libc].\nCan be used with multiple mappings [e.g libc heap stack]",
+)
 parser.add_argument(
-    "page_names", type=str, nargs="*", default=[], help="Mapping to search [e.g. libc].\nCan be used with multiple mappings [e.g libc heap stack]")
-parser.add_argument(
-    "--save-as", type=str, default=None, help="Sets the filename for the output of this command [e.g. --save-as='out.txt']")
+    "--save-as",
+    type=str,
+    default=None,
+    help="Sets the filename for the output of this command [e.g. --save-as='out.txt']",
+)
 
 @pwndbg.commands.ArgparsedCommand(parser, category=CommandCategory.LINUX)
 @pwndbg.commands.OnlyWhenRunning
-def strings(n: int=4, page_names: List[str]=[], save_as: str=None):
+def strings(n: int = 4, page_names: List[str] = [], save_as: str = None):
     # Extract pages with PROT_READ permission
     readable_pages: List[Page] = [page for page in pwndbg.aglib.vmmap.get() if page.read]
 
     for page in readable_pages:
         if page_names and not any(name in page.objfile for name in page_names):
-            continue # skip if page does not belong to any of the specified mappings
+            continue  # skip if page does not belong to any of the specified mappings
 
         count = page.memsz
         start_address = page.vaddr
@@ -39,8 +49,8 @@ def strings(n: int=4, page_names: List[str]=[], save_as: str=None):
             continue  # skip if access is denied
 
         # all strings in the `data`
-        strings: List[bytes] = re.findall(rb'[ -~]{%d,}' % n, data)
-        decoded_strings: List[str] = [s.decode('ascii', errors='ignore') for s in strings]
+        strings: List[bytes] = re.findall(rb"[ -~]{%d,}" % n, data)
+        decoded_strings: List[str] = [s.decode("ascii", errors="ignore") for s in strings]
 
         if not save_as:
             for string in decoded_strings:
@@ -48,4 +58,4 @@ def strings(n: int=4, page_names: List[str]=[], save_as: str=None):
             continue
 
         with open(save_as, "w") as f:
-           f.writelines(string + "\n" for string in decoded_strings)
+            f.writelines(string + "\n" for string in decoded_strings)
